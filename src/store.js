c10bfae4e9f8fa3af987e5725665467a9c8a78d4
Satya Prakash <98944718+satyaprakash9@users.noreply.github.com>
2024-02-06 10:31:13 +0530

    Delete src directory

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import quizReducer from "./features/quizSlice";
-
-export const store = configureStore({
-    reducer: {
-        quiz: quizReducer,
-    }
-});
